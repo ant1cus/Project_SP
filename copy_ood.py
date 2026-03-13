3e55d133e49d307c0a04b8a7654325c43e9b2237
ant1cus <ant1cus@yandex.ru>
2026-03-13 09:36:14 +0300

    add time for each file

--- copy_ood.py
+++ copy_ood.py
@@ -24,7 +24,7 @@ def copy_files(incoming_data: dict, current_progress, now_doc, all_doc, line_doi
 			df.fillna(value={2: incoming_data['name_set']}, inplace=True)
 		df[1] = df[1].astype(str)
 		df[3] = df[3].astype(str)
-		df['finish_folder'] = str(Path(incoming_data['finish_path'], name_finish_folder)) + '\\' + df[1] + '\\' + df[3]
+		df['finish_folder'] = str(Path(incoming_data['finish_path'], name_finish_folder)) + '\\' + df[2] + '\\' + df[3]
 		serial_nums = {}
 		logging.info(f"загрузка excel - {datetime.now() - start_time}")
 		# Для кол-ва копий можно будет потом расскоментировать
@@ -48,12 +48,14 @@ def copy_files(incoming_data: dict, current_progress, now_doc, all_doc, line_doi
 				# find_files.update(full_find)
 		percent = 100 / len(serial_nums)
 		logging.info(f"считывание excel - {datetime.now() - start_time}")
+
 		for file in Path(incoming_data['start_path']).rglob('*.*'):
 			if window_check.stop_threading:
 				return {'status': 'cancel', 'trace': '', 'text': ''}
 			file_sn = file.stem.partition('_')[2].partition('_')[2].partition('_')[0]
 			if file_sn not in serial_nums:
 				continue
+			start_time_file = datetime.now()
 			if Path(serial_nums[file_sn], 'photo', file.name).exists():
 				continue
 			if not Path(serial_nums[file_sn], 'photo').exists():
@@ -64,6 +66,7 @@ def copy_files(incoming_data: dict, current_progress, now_doc, all_doc, line_doi
 			current_progress += percent
 			line_progress.emit(f'Выполнено {int(current_progress)} %')
 			progress_value.emit(int(current_progress))
+			logging.info(f"отдельный файл - {datetime.now()-start_time_file}")
 		logging.info(f"конец программы - {datetime.now() - start_time}")
 		return {'status': 'success', 'text': '', 'trace': ''}
 	except BaseException as ex:
